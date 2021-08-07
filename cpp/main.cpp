a94f06b7c611d564e758d97092bc4174aa506294
dk-raw <ddkatsios@protonmail.com>
2021-08-07 10:07:05 +0300

    meow

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <string>
 #include "loop.h"
 #include "average.h"
 using namespace std;
@@ -8,16 +9,17 @@ using namespace chrono;
 int main()
 {
     deque<int> LOOP_TIMES = {};
+    string AVERAGE_TIMES[2][1] = {};
     int i = 0;
     cout << endl
          << endl
          << "DO WHILE LOOP 1 MILLION TIMES" << endl;
     do
     {
-        auto start = high_resolution_clock::now();
+        auto START = high_resolution_clock::now();
         LOOP::DO_LOOP_1MILL_TIMES();
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop - start);
+        auto STOP = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(STOP - START);
         LOOP_TIMES.push_front(duration.count());
         if (i == 9)
             cout << duration.count() << endl;
@@ -26,6 +28,8 @@ int main()
         i++;
     } while (i < 10);
     cout << "AVERAGE:" << AVERAGE::CALCULATE(LOOP_TIMES) << endl;
+    AVERAGE_TIMES[0][0] = "DO WHILE LOOP";
+    AVERAGE_TIMES[0][1] = to_string(AVERAGE::CALCULATE(LOOP_TIMES));
 
     cout << endl
          << endl
@@ -35,10 +39,10 @@ int main()
     i = 0;
     do
     {
-        auto start = high_resolution_clock::now();
+        auto START = high_resolution_clock::now();
         LOOP::WHILE_LOOP_1MILL_TIMES();
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop - start);
+        auto STOP = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(STOP - START);
         LOOP_TIMES.push_front(duration.count());
         if (i == 9)
             cout << duration.count() << endl;
@@ -47,6 +51,8 @@ int main()
         i++;
     } while (i < 10);
     cout << "AVERAGE:" << AVERAGE::CALCULATE(LOOP_TIMES) << endl;
+    AVERAGE_TIMES[1][0] = "WHILE LOOP";
+    AVERAGE_TIMES[1][1] = to_string(AVERAGE::CALCULATE(LOOP_TIMES));
 
     cout << endl
          << endl
@@ -56,10 +62,10 @@ int main()
     i = 0;
     do
     {
-        auto start = high_resolution_clock::now();
+        auto START = high_resolution_clock::now();
         LOOP::WHILE_LOOP_1MILL_TIMES();
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop - start);
+        auto STOP = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(STOP - START);
         LOOP_TIMES.push_front(duration.count());
         if (i == 9)
             cout << duration.count() << endl;
@@ -68,4 +74,10 @@ int main()
         i++;
     } while (i < 10);
     cout << "AVERAGE:" << AVERAGE::CALCULATE(LOOP_TIMES) << endl;
+    AVERAGE_TIMES[2][0] = "FOR WHILE";
+    AVERAGE_TIMES[2][1] = to_string(AVERAGE::CALCULATE(LOOP_TIMES));
+
+    cout << endl
+         << endl;
+    //print the averages
 }
